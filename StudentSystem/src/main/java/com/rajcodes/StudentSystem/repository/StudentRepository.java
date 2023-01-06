f2bd11a586a594e51a82cf7c2774c2bfb40e747c
Raj Pratap Singh Tomar <59117868+rajtomarmanitcse@users.noreply.github.com>
2023-01-06 19:26:54 +0530

    sql connection done

--- StudentSystem/src/main/java/com/rajcodes/StudentSystem/repository/StudentRepository.java
+++ StudentSystem/src/main/java/com/rajcodes/StudentSystem/repository/StudentRepository.java
@@ -1,5 +1,10 @@
 package com.rajcodes.StudentSystem.repository;
 
-public interface StudentRepository {
+import com.rajcodes.StudentSystem.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Integer> {
 
 }
