de15ca480c73c2baa2a173c0fe709d47624375df
Raj Pratap Singh Tomar <59117868+rajtomarmanitcse@users.noreply.github.com>
2023-01-07 15:41:09 +0530

    backend finished

--- StudentSystem/src/main/java/com/rajcodes/StudentSystem/controller/StudentController.java
+++ StudentSystem/src/main/java/com/rajcodes/StudentSystem/controller/StudentController.java
@@ -1,4 +1,26 @@
 package com.rajcodes.StudentSystem.controller;
 
+import com.rajcodes.StudentSystem.model.Student;
+import com.rajcodes.StudentSystem.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/student")
 public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody Student student){
+        studentService.saveStudent(student);
+        return "New Student is Added";
+    }
+
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
+    }
 }
--- StudentSystem/src/main/java/com/rajcodes/StudentSystem/service/StudentServiceImpl.java
+++ StudentSystem/src/main/java/com/rajcodes/StudentSystem/service/StudentServiceImpl.java
@@ -1,5 +1,24 @@
 package com.rajcodes.StudentSystem.service;
 
-public class StudentServiceImpl {
+import com.rajcodes.StudentSystem.model.Student;
+import com.rajcodes.StudentSystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+    @Autowired
+    private StudentRepository repository;
+
+    @Override
+    public Student saveStudent(Student student) {
+        return repository.save(student);
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return repository.findAll();
+    }
 }
